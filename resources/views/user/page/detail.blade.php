13fea3e3d6565c7c243ae2d93989fd74e15de04a
truongnguyen777t <truongnguyen777t@gmail.com>
2024-01-24 09:54:54 +0700

    Detail

--- resources/views/user/page/detail.blade.php
+++ resources/views/user/page/detail.blade.php
@@ -112,26 +112,21 @@
                          <!-- Leave a comment -->
                          <div>
                               <h4 class="f1-l-4 cl3 p-b-12">
-                                   Leave a Comment
+                              Để lại một bình luận
                               </h4>
 
                               <p class="f1-s-13 cl8 p-b-40">
-                                   Your email address will not be published. Required fields are marked *
+                                   Địa chỉ email của bạn sẽ không được công bố. Các trường bắt buộc được đánh dấu *
                               </p>
 
-                              <form>
+                              <form action="{{ route('user.comment.add') }}" method="POST">
+                              {{ csrf_field() }}
                                    <textarea class="bo-1-rad-3 bocl13 size-a-15 f1-s-13 cl5 plh6 p-rl-18 p-tb-14 m-b-20"
-                                        name="msg" placeholder="Comment..."></textarea>
-
+                                        name="noidung" id="noidung" required placeholder="Comment..."></textarea>
                                    <input class="bo-1-rad-3 bocl13 size-a-16 f1-s-13 cl5 plh6 p-rl-18 m-b-20"
-                                        type="text" name="name" placeholder="Name*">
-
+                                        type="text" name="name" required placeholder="Name*">
                                    <input class="bo-1-rad-3 bocl13 size-a-16 f1-s-13 cl5 plh6 p-rl-18 m-b-20"
-                                        type="text" name="email" placeholder="Email*">
-
-                                   <input class="bo-1-rad-3 bocl13 size-a-16 f1-s-13 cl5 plh6 p-rl-18 m-b-20"
-                                        type="text" name="website" placeholder="Website">
-
+                                        type="text" name="email" required placeholder="Email*">
                                    <button class="size-a-17 bg2 borad-3 f1-s-12 cl0 hov-btn1 trans-03 p-rl-15 m-t-10">
                                         Post Comment
                                    </button>
