3bb14767954cc75b0e637be499a870f662861a64
truongnguyen777t <truongnguyen777t@gmail.com>
2024-01-24 10:02:49 +0700

    Migration tbluser, tblbinhluan

---
+++ database/migrations/2024_01_23_072927_create_tbluser_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tbluser', function (Blueprint $table) {
+            $table->id('IDUS');
+            $table->string('TenUS', 100);
+            $table->string('MatKhauUS', 200);
+            $table->string('EmailUS', 50);
+            $table->tinyInteger('TrangThaiUS');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tbluser');
+    }
+};
